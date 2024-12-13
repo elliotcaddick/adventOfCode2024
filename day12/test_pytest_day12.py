a3ad60ac47a42c278a1d2880652a3ff7b99576e4
Elliot Caddick <elliot.caddick@outlook.fr>
2024-12-13 09:24:57 +0100

    [D12] Part 2 - OK

--- day12/test_pytest_day12.py
+++ day12/test_pytest_day12.py
@@ -5,3 +5,15 @@ def test_part_one_1():
     
 def test_part_one_2():
     assert d12.part_one("day12/test/sample2.txt") == 1930
+    
+def test_part_two_1():
+    assert d12.part_two("day12/test/sample.txt") == 80
+    
+def test_part_two_2():
+    assert d12.part_two("day12/test/sample3.txt") == 236
+    
+def test_part_two_3():
+    assert d12.part_two("day12/test/sample4.txt") == 368
+
+def test_part_two_4():
+    assert d12.part_two("day12/test/sample2.txt") == 1930
