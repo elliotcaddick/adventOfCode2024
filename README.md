d71188259e42efb6998c8b2e07f907eddf40c0e8
Elliot Caddick <elliot.caddick@outlook.fr>
2024-12-14 16:16:11 +0100

    [D14] Part 2 - OK

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Advent Of Code 2024
 
-27/50 ⭐ 
+28/50 ⭐ 
 
 ### Quest 1
 - Part 1 : $${\color{green}OK}$$
@@ -56,7 +56,7 @@
 
 ### Quest 14
 - Part 1 : $${\color{green}OK}$$
-- Part 2 : $${\color{grey}N/A}$$
+- Part 2 : $${\color{green}OK}$$
 
 ### Quest 15
 - Part 1 : $${\color{grey}N/A}$$
--- day14/main.py
+++ day14/main.py
@@ -64,4 +64,43 @@ def part_one(filename, wide, tall):
 # print(f"Part 1: {part_one("input/puzzle.txt", 101, 103)}")
 
 
-# print(f"Part 2: {part_two("input/puzzle.txt")}")
+import re
+from itertools import count
+from math import prod
+
+
+def parse_input():
+    with open("input/puzzle.txt", "r") as file:
+        data = file.read()
+    pattern = r"p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)"
+    return [list(map(int, x)) for x in re.findall(pattern, data)]
+
+
+ROBOTS = parse_input()
+N, M = 103, 101
+
+
+def step(j, i, dj, di, t):
+    di *= t
+    dj *= t
+    i = (i + di) % N
+    j = (j + dj) % M
+    return i, j
+
+
+def draw(positions):
+    grid = [["."] * M for _ in range(N)]
+    for i, j in positions:
+        grid[i][j] = "\u2588"
+    print("\n".join("".join(row) for row in grid))
+
+
+def part_two():
+    for t in count():
+        cur = {step(*robot, t) for robot in ROBOTS}
+        if len(cur) == len(ROBOTS):
+            draw(cur)
+            return t
+
+
+# print(f"Part 2: {part_two()}")
